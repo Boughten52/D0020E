31fcd14f5004a487c171a2c78b58780c7c84bad2
Lucke0011 <lukas.ledin@gmail.com>
2022-01-19 15:55:14 +0100

    Input and Output

---
+++ Input/FibaroInput.py
@@ -0,0 +1,174 @@
+# !/usr/bin/env python3.6
+# -*- coding: utf-8 -*-
+#
+#  Copyright 2014 Klaudiusz Staniek
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+import logging
+import time
+
+from fiblary3.client import Client
+from fiblary3.common import exceptions
+
+logging.basicConfig(
+    format='%(asctime)-15s %(levelname)s: %(module)s:%(funcName)s'
+           ':%(lineno)d: %(message)s',
+    level=logging.CRITICAL)
+
+
+def main():
+    hc2 = Client(
+        'v3',
+        'http://130.240.114.44/api/',
+        'unicorn@ltu.se',
+        'Ks2mpH2Al!'
+    )
+
+    rooms = dict((room.id, room.name) for room in hc2.rooms.list())
+    for room in rooms.items():
+        print(room)
+
+    def get_room_name_from_id(room_id):
+        try:
+            return rooms[room_id]
+        except KeyError:
+            return "Not assigned"
+
+    start = time.time()
+
+
+
+    open_doors = hc2.devices.list(
+        baseType = "com.fibaro.doorWindowSensor"
+        #properties = "parameters[id==1]"
+
+    )
+
+    print("Open doors:")
+    for device in open_doors:
+        room_id = device.roomID
+        room_name = get_room_name_from_id(room_id)
+        print("  {}({}) in {}".format(device.name, device.id, room_name))
+
+    stop = time.time()
+    print("Command execution time: {0:.2f}s".format(stop - start))
+
+    start = time.time()
+    open_doors = hc2.devices.list(
+        type="door_sensor",
+        jsonpath="$[?(@.properties.value=='1'"
+                 " && @.properties.disabled=='0')]")
+
+    print("\nOpen doors:")
+    for device in open_doors:
+        room_id = device.roomID
+        room_name = get_room_name_from_id(room_id)
+        print("  {}({}) in {}".format(device.name, device.id, room_name))
+
+    stop = time.time()
+    print("Command execution time: {0:.2f}s".format(stop - start))
+
+    start = time.time()
+    open_windows = hc2.devices.list(
+        type="window_sensor",
+        p_value="1",
+        p_disabled="0"
+    )
+
+    print("\nOpen windows:")
+    for device in open_windows:
+        room_id = device.roomID
+        room_name = get_room_name_from_id(room_id)
+        print("  {}({}) in {}".format(device.name, device.id, room_name))
+
+    stop = time.time()
+    print("Command execution time: {0:.2f}s".format(stop - start))
+
+    start = time.time()
+    open_windows = hc2.devices.list(
+        type="window_sensor",
+        jsonpath="$[?(@.properties.value=='1'"
+                 " && @.properties.disabled=='0')]")
+
+    print("\nOpen windows:")
+    for device in open_windows:
+        room_id = device.roomID
+        room_name = get_room_name_from_id(room_id)
+        print("  {}({}) in {}".format(device.name, device.id, room_name))
+
+    stop = time.time()
+    print("Command execution time: {0:.2f}s".format(stop - start))
+
+    start = time.time()
+    lights = hc2.devices.list(p_disabled="0", p_isLight="1")
+
+    print("\nLights:")
+    for device in lights:
+        room_id = device.roomID
+        room_name = get_room_name_from_id(room_id)
+        state = "on" if device.properties['value'] == "1" else "off"
+        print("  {}({}) in {} is {}".format(
+            device.name,
+            device.id,
+            room_name,
+            state))
+
+    stop = time.time()
+    print("Command execution time: {0:.2f}s".format(stop - start))
+
+    start = time.time()
+    lights = hc2.devices.list(
+        jsonpath="$[?(@.properties.disabled=='0'"
+                 " && @.properties.isLight=='1')]")
+
+    print("\nLights:")
+    for device in lights:
+        room_id = device.roomID
+        room_name = get_room_name_from_id(room_id)
+        state = "on" if device.properties['value'] == "1" else "off"
+        print("  {}({}) in {} is {}".format(
+            device.name,
+            device.id,
+            room_name,
+            state))
+
+    stop = time.time()
+    print("Command execution time: {0:.2f}s".format(stop - start))
+
+    start = time.time()
+    try:
+        device = hc2.devices.find(name='Blue light')
+        value = device.properties.value
+        if value == '0':
+            device.turnOn()
+            time.sleep(2)
+            device.setValue(10)
+            print("\nBlue Light turned on")
+        else:
+            device.turnOff()
+            print("\nBlue Light turned off")
+
+    except exceptions.NotFound:
+        print("\nBlue Light not found")
+    stop = time.time()
+    print("Command execution time: {0:.2f}s".format(stop - start))
+
+    #try:
+    #    device = hc2.devices.find(type='dimmable_light')
+    #except exceptions.NoUniqueMatch:
+    #    print("\nMore then one device matching the criteria")
+
+
+if __name__ == '__main__':
+    main()
--- output/phueOutput.py
+++ output/phueOutput.py
@@ -1,13 +1,35 @@
+import random
+from time import sleep
+
 from phue import Bridge
 
 bridge = Bridge('130.240.114.9')
+#bridge = Bridge('130.240.114.26') Fel
 
 # Connect once and press the connect button on the Pilips Hue bridge at the same time
 bridge.connect()
 
+print("HEJ")
+
 # Might be preferable to get the names instead of id:s
-lights = bridge.get_light_objects('id')
-lights[1].on = True
-lights[1].brightness = 127
+#lights = bridge.get_light_objects('id')
+#{13: <phue.Light object "Hue color light 1" at 0x7ffa10ab4fd0>, 14: <phue.Light object "Hue color light 2" at 0x7ffa10ad05d0>, 15: <phue.Light object "Hue color light 3" at 0x7ffa138a5950>}
+
+#Tak ovanför dörr
+#lights[13].on = True
+while True:
+	lights = bridge.get_light_objects()
+	for light in lights:
+		light.brightness = 254
+		light.xy = [random.random(),random.random()]
+	sleep(10)
+
+#Tak ovanför mikro
+lights[14].on = True
+lights[14].brightness = 255
+
+#Lampa på tvbänken
+lights[15].on = True
+lights[15].brightness = 0
 
 print(lights)
