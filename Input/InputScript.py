8e5dd71810669149c3b8639a77de5df0ce4116af
Boughten52 <jonatanstenlund99@gmail.com>
2022-01-28 08:11:54 +0100

    Added get_state function to Fibaro

--- Input/InputScript.py
+++ Input/InputScript.py
@@ -1,9 +1,9 @@
 from abc import ABC, abstractmethod
 import json
 
+
 class Input(ABC):
 
     @abstractmethod
-    def get_input(self) -> (bool, str):
+    def get_state(self) -> (bool, str):
         pass
-        
