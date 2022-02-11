bbac8f99dac0eeb19d66bf44129e12e69b0a60d4
Lucke0011 <lukas.ledin@gmail.com>
2022-02-11 15:18:21 +0100

    Everything is now a class

--- TESTING/config/buildConfigFile.py
+++
@@ -1,9 +0,0 @@
-import configparser
-
-config = configparser.ConfigParser()
-
-config.add_section('user')
-config.set('user', 'admin', 'Jonatan')
-
-with open('config.ini', 'w') as configfile:
-    config.write(configfile)
--- TESTING/config/readConfigFile.py
+++
@@ -1,7 +0,0 @@
-import configparser
-
-config = configparser.ConfigParser()
-config.read('config.ini')
-userInfo = config["user"]
-admin = userInfo["admin"]
-print(admin)
